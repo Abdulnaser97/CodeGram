cb873c2064a7eda09af98eda793c40feca389d7a
Wasif <wasifz9@gmail.com>
2022-01-01 10:25:31 -0500

    removing console log

--- client/src/SourceDoc/SourceDoc.js
+++ client/src/SourceDoc/SourceDoc.js
@@ -78,7 +78,6 @@ function SourceDoc(props) {
   const [sourceFiles, setSourceFiles] = useState(null)
   const [path, setPath] = useState([])
   const [pathComponent, setPathComponent] = useState(null)
-  console.log(selectedFile)
   useEffect(() => {
     if (props.data.repo) {
       var homePath = {
