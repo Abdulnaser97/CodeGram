f1391ddc332819973c3f37f6a020636caaff9501
JuliaATaylor <91855053+JuliaATaylor@users.noreply.github.com>
2022-03-21 14:12:12 -0400

    added accent colours and increased saturation

--- client/src/Themes.js
+++ client/src/Themes.js
@@ -3,13 +3,17 @@ import { createTheme } from "@mui/material/styles";
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "#FFAEA6",
+      main: "#FF6161",
+      originalPink:"#FFAEA6",
       pinkerPink: "#ff8b8b",
+      saturatedPink: "#FF6161",
       grey: "#d4d4d4",
       darkGrey: "#8f8f8f",
       lightGrey: "#adadad",
       darkestGrey: "#767676",
       deepestDarkestGrey: "#4d4d4d",
+      orangeAccent: "#FFC300",
+      sageAccent: "#73C6B6",
     },
     secondary: {
       main: "#ffadad",
